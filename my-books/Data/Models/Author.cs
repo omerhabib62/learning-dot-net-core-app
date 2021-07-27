4073a60760ebb95d3c382f70de83add02347308e
omerhabib62 <omer.habib62@gmail.com>
2021-07-27 21:02:36 +0500

    Changes in migrations and also added some models (Author, Book_author)

--- my-books/Data/Models/Author.cs
+++ my-books/Data/Models/Author.cs
@@ -7,5 +7,9 @@ namespace my_books.Data.Models
 {
     public class Author
     {
+        public int Id { get; set; }
+        public string FullName { get; set; }
+        //Navigation Property
+        public List<Book_Author> Book_Authors { get; set; }
     }
 }
---
+++ my-books/Migrations/20210727155408_AuthorAndBookAuthors_foriegnKeys.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace my_books.Migrations
+{
+    public partial class AuthorAndBookAuthors_foriegnKeys : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateIndex(
+                name: "IX_Book_Authors_AuthorId",
+                table: "Book_Authors",
+                column: "AuthorId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Book_Authors_BookId",
+                table: "Book_Authors",
+                column: "BookId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Books_Publishers_PublisherId",
+                table: "Books",
+                column: "PublisherId",
+                principalTable: "Publishers",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Books_Publishers_PublisherId",
+                table: "Books");
+        }
+    }
+}
